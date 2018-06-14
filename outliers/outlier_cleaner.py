e7f4cf50865509fd9340ed5a6f991a7a11dddd40
Yuan Zhang <yuan.zhang5@mail.mcgill.ca>
2018-06-13 20:37:23 -0400

    completed outliers mini project

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -7,14 +7,15 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
-    cleaned_data = []
 
-    ### your code goes here
+    #cleaned_data = []
+    # construct a data list with age, net_worth in place plus one extra param for square error
+    data = [(age, net_worth, pred - net_worth) for age, net_worth, pred in zip (ages, net_worths, predictions)]
+    sorted_data = sorted(data,key=lambda tup:tup[2])
+    # Clean away the 10% of points with largest residual errors
+    cleaned_data = sorted_data[:-9]
 
-    
     return cleaned_data
-
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -7,12 +7,9 @@ import pickle
 
 from outlier_cleaner import outlierCleaner
 
-
 ### load up some practice data with outliers in it
 ages = pickle.load( open("practice_outliers_ages.pkl", "r") )
 net_worths = pickle.load( open("practice_outliers_net_worths.pkl", "r") )
-
-
 
 ### ages and net_worths need to be reshaped into 2D numpy arrays
 ### second argument of reshape command is a tuple of integers: (n_rows, n_columns)
@@ -20,20 +17,18 @@ net_worths = pickle.load( open("practice_outliers_net_worths.pkl", "r") )
 ### and n_columns is the number of features
 ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
 net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
+
 from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LinearRegression
+
 ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages, net_worths, test_size=0.1, random_state=42)
 
-### fill in a regression here!  Name the regression object reg so that
-### the plotting code below works, and you can see what your regression looks like
-
-
-
-
-
-
-
-
-
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
+print "slope:", reg.coef_
+print "intercept:", reg.intercept_
+print "training score:", reg.score(ages_train, net_worths_train)
+print "testing score:", reg.score(ages_test, net_worths_test)
 
 
 try:
@@ -42,7 +37,6 @@ except NameError:
     pass
 plt.scatter(ages, net_worths)
 plt.show()
-
 
 ### identify and remove the most outlier-y points
 cleaned_data = []
@@ -53,12 +47,6 @@ except NameError:
     print "your regression object doesn't exist, or isn't name reg"
     print "can't make predictions to use in identifying outliers"
 
-
-
-
-
-
-
 ### only run this code if cleaned_data is returning data
 if len(cleaned_data) > 0:
     ages, net_worths, errors = zip(*cleaned_data)
@@ -68,6 +56,9 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        print "new slope:", reg.coef_
+        print "new training score:", reg.score(ages_train, net_worths_train)
+        print "new testing score:", reg.score(ages_test, net_worths_test)
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
         print "you don't seem to have regression imported/created,"
@@ -81,4 +72,3 @@ if len(cleaned_data) > 0:
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
-
