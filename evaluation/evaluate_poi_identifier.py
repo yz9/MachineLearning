2d5a7301e4089b82c1e9a6893c3c8bd3c7829375
Yuan Zhang <yuan.zhang5@mail.mcgill.ca>
2018-07-19 20:23:25 -0400

    added code for finding number of predicted POIs in test set

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -15,6 +15,9 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn import cross_validation
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -24,8 +27,16 @@ features_list = ["poi", "salary"]
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
+### randomly split into training and test sets
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(
+    features, labels, test_size=0.3, random_state=42)
 
+### create decision tree classifier
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print "accuracy score:", accuracy_score(labels_test, pred)
 
-### your code goes here 
-
-
+predicted_poi = [poi for poi in pred if poi==1]
+print "predicted POTs:", len(predicted_poi)
+print "total number in test set", len(features_test)
